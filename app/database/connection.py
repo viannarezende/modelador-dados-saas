badc242fe9bf2bfecc63795f7a18363938d3eebb
Carol Vianna <viannarezende@gmail.com>
2026-05-27 23:09:40 -0300

    fix database url render

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,7 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://app_user:app_password@localhost:5432/app_db"
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL)
 
